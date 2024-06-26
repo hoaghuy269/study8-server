d7be9f4db253273eed66c4029ad716fc9b39c95b
HuyNH <huynh@unit.com.vn>
2024-06-26 11:57:01 +0700

    [HuyNH] Fix log warn

--- study8-server/study8-sys/src/main/java/com/study8/sys/config/WebClientConfig.java
+++ study8-server/study8-sys/src/main/java/com/study8/sys/config/WebClientConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+/**
+ * WebClientConfig
+ * @Date: 2024-06-26
+ * @Author: HuyNH
+ * @Desc: Webclient config
+ */
 @Configuration
 public class WebClientConfig {
     @Bean
