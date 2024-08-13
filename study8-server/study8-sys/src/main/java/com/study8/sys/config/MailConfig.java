f0dc5ba828f4ee98fe75de291e6fa84e6e0fecb0
HuyNH <huynh@unit.com.vn>
2024-08-13 14:30:58 +0700

    [HuyNH] Fix Sonar java:S1128

--- study8-server/study8-sys/src/main/java/com/study8/sys/config/MailConfig.java
+++ study8-server/study8-sys/src/main/java/com/study8/sys/config/MailConfig.java
@@ -2,7 +2,6 @@ package com.study8.sys.config;
 
 import com.study8.sys.constant.SysConstant;
 import com.study8.sys.system.service.SystemConfigService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
