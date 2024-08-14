d9145e8b78cfc9a4514913acf971df2f1fd8aa03
HuyNH <huynh@unit.com.vn>
2024-08-14 15:29:23 +0700

    [HuyNH] Remove hash code

--- study8-server/study8-gateway/src/main/java/com/study8/gateway/constant/APIConstant.java
+++
@@ -1,4 +0,0 @@
-package com.study8.gateway.constant;
-
-public class APIConstant {
-}
---
+++ study8-server/study8-gateway/src/main/java/com/study8/gateway/constant/GatewayConstant.java
@@ -0,0 +1,16 @@
+package com.study8.gateway.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * GatewayConstant
+ * @Date: 2024-08-14
+ * @Author: HuyNH
+ * @Desc: Gateway Constant
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GatewayConstant {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+}
