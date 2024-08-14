db317fe78060d257aaf63e90db8a75fbb1924e8a
HuyNH <huynh@unit.com.vn>
2024-08-14 14:38:32 +0700

    [HuyNH] Fix sonar java:S1118

--- study8-server/study8-gateway/src/main/java/com/study8/gateway/config/GatewayConfig.java
+++ study8-server/study8-gateway/src/main/java/com/study8/gateway/config/GatewayConfig.java
@@ -1,7 +1,6 @@
 package com.study8.gateway.config;
 
 import com.study8.gateway.security.AuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
