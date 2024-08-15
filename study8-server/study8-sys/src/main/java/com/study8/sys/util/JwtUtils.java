074d2f82824408964ac5d43705a538ae3f323ba8
HuyNH <huynh@unit.com.vn>
2024-08-15 11:23:43 +0700

    [HuyNH] Fix sonarlint

--- study8-server/study8-sys/src/main/java/com/study8/sys/util/JwtUtils.java
+++ study8-server/study8-sys/src/main/java/com/study8/sys/util/JwtUtils.java
@@ -1,14 +1,11 @@
 package com.study8.sys.util;
 
-import com.study8.sys.constant.SysConstant;
 import com.study8.sys.service.UserDetailsImpl;
-import com.study8.sys.system.service.SystemConfigService;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
