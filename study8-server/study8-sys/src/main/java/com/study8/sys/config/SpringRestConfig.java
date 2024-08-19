fb3e894ae1788894daa158449253683871a555a7
HuyNH <huynh@unit.com.vn>
2024-08-19 13:17:36 +0700

    [HuyNH-HF] Add java doc

--- study8-server/study8-sys/src/main/java/com/study8/sys/config/SpringRestConfig.java
+++ study8-server/study8-sys/src/main/java/com/study8/sys/config/SpringRestConfig.java
@@ -5,6 +5,12 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+/**
+ * SpringRestConfig
+ * @Date: 2024-08-19
+ * @Author: HuyNH
+ * @Desc: Spring Rest Config
+ */
 @Configuration
 public class SpringRestConfig implements RepositoryRestConfigurer {
     @Override
