6c9143907f97c7f87db5c960b221a492b0fe8ec6
HuyNH <huynh@unit.com.vn>
2024-08-19 13:15:59 +0700

    [HuyNH-HF] Hotfix auto expose api config

---
+++ study8-server/study8-sys/src/main/java/com/study8/sys/config/SpringRestConfig.java
@@ -0,0 +1,16 @@
+package com.study8.sys.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class SpringRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config,
+            CorsRegistry cors) {
+        config.disableDefaultExposure();
+    }
+}
