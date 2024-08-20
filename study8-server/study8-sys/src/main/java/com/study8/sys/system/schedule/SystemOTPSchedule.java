230222ea60827bf3321312eaeb2afec0d18acddc
HuyNH <huynh@unit.com.vn>
2024-08-20 10:55:12 +0700

    [HuyNH-HF] Fix sonar

--- study8-server/study8-sys/src/main/java/com/study8/sys/system/schedule/SystemOTPSchedule.java
+++ study8-server/study8-sys/src/main/java/com/study8/sys/system/schedule/SystemOTPSchedule.java
@@ -3,8 +3,6 @@ package com.study8.sys.system.schedule;
 import com.study8.sys.system.entity.SystemOTP;
 import com.study8.sys.system.service.SystemOTPService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
