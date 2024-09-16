3f63952108f4738e6bee358c2dc3e3f8af7ec490
HuyNH <huynh@unit.com.vn>
2024-09-16 10:14:31 +0700

    [HuyNH] Add role visitor - api register

--- study8-server/study8-sys/src/main/java/com/study8/sys/auth/enumf/RoleEnum.java
+++ study8-server/study8-sys/src/main/java/com/study8/sys/auth/enumf/RoleEnum.java
@@ -1,11 +1,33 @@
 package com.study8.sys.auth.enumf;
 
+import lombok.Getter;
+
 /**
  * RoleEnum
  * @Date: 2024-06-01
  * @Author: HuyNH
  * @Desc: Roles of system
  */
+@Getter
 public enum RoleEnum {
-    ROLE_USER, ROLE_ADMIN
+    ROLE_ADMIN("ROLE_ADMIN"), //Admin
+    ROLE_STUDENT("ROLE_STUDENT"), //Học sinh
+    ROLE_TEACHER("ROLE_TEACHER"), //Giáo viên
+    ROLE_VISITOR("ROLE_VISITOR"), //Khách vãn lai
+    UNKNOWN("UNKNOWN"); //Unknown
+
+    private final String value;
+
+    RoleEnum(String value) {
+        this.value = value;
+    }
+
+    public static RoleEnum resolveByValue(String value) {
+        for (RoleEnum enumValue : RoleEnum.values()) {
+            if (enumValue.value.equals(value)) {
+                return enumValue;
+            }
+        }
+        return UNKNOWN;
+    }
 }
