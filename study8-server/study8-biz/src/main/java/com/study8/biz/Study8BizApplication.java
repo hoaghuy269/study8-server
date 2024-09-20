d87b71a3f096ec4427aa70c466624bd31472bbaa
HuyNH <huynh@unit.com.vn>
2024-09-20 13:31:36 +0700

    [HuyNH] Add module study8-biz

---
+++ study8-server/study8-biz/src/main/java/com/study8/biz/Study8BizApplication.java
@@ -0,0 +1,13 @@
+package com.study8.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study8BizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study8BizApplication.class, args);
+    }
+
+}
---
+++ study8-server/study8-biz/src/test/java/com/study8/biz/Study8BizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study8.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study8BizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
