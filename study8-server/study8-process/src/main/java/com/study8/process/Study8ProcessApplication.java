0d5759f16535eac80e89c72c5c79e0907cce6bee
HuyNH <huynh@unit.com.vn>
2024-10-17 11:40:48 +0700

    [HuyNH] Add study8-process module

---
+++ study8-server/study8-process/src/main/java/com/study8/process/Study8ProcessApplication.java
@@ -0,0 +1,13 @@
+package com.study8.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study8ProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study8ProcessApplication.class, args);
+	}
+
+}
