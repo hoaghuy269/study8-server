06c374ee82bfa219bde12997888b6edaa5940b06
HuyNH <huynh@unit.com.vn>
2024-10-21 08:57:58 +0700

    [HuyNH] Update version Camunda, Bug Camunda version conflict with Spring Eureka Client

--- study8-server/study8-process/src/main/java/com/study8/process/Study8ProcessApplication.java
+++ study8-server/study8-process/src/main/java/com/study8/process/Study8ProcessApplication.java
@@ -1,9 +1,13 @@
 package com.study8.process;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableProcessApplication
+@EnableDiscoveryClient
 public class Study8ProcessApplication {
 
 	public static void main(String[] args) {
